9a42bd147803948a09f118a129c683bcd75f5162
dmitrynogin <dmitryn@mail.com>
2016-09-15 18:54:40 -0700

    Does FareySequence implement F4?

---
+++ FareySequences.UnitTests/FareySequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace FareySequences.UnitTests
+{
+    [TestClass]
+    public class FareySequence_Should
+    {
+        [TestMethod]
+        public void Implement_F4()
+        {
+            var f4 = new[]
+            {
+                new RationalNumber(0, 1),
+                new RationalNumber(1, 4),
+                new RationalNumber(1, 3),
+                new RationalNumber(1, 2),
+                new RationalNumber(2, 3),
+                new RationalNumber(3, 4),
+                new RationalNumber(1, 1)
+            };
+
+            var result = new FareySequence(4)
+                .ToArray();
+
+            CollectionAssert.AreEqual(f4, result);
+        }
+    }
+}
---
+++ FareySequences/FareySequence.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FareySequences
+{
+    public class FareySequence : IEnumerable<RationalNumber>
+    {
+        public FareySequence(int order)
+        {
+
+        }
+
+        public IEnumerator<RationalNumber> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
