562aa8b7b76bcf2eb64911eede7c860f26f9f7e1
dmitrynogin <dmitryn@mail.com>
2016-09-15 18:05:33 -0700

    Does RationalNumber represent Numerator and Denominator?

---
+++ FareySequences.UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FareySequences.UnitTests
+{
+    [TestClass]
+    public class RationalNumber_Should
+    {
+        [TestMethod]
+        public void Represent_Numerator_And_Denomerator()
+        {
+            var rn = new RationalNumber(33, 44);
+
+            Assert.AreEqual("33/44", rn.ToString());
+        }
+    }
+}
---
+++ FareySequences/RationalNumber.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FareySequences
+{
+    public class RationalNumber 
+    {
+        public RationalNumber(int numerator, int denominator)
+        {
+        }
+
+        public override string ToString() => null;
+        int Numerator { get; }
+        int Denomerator { get; }
+    }
+}
