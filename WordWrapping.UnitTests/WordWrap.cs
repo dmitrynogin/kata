0126095aba4d0af4b2ff449fa3234e1280e2939f
dmitrynogin <dmitryn@mail.com>
2016-09-15 21:02:24 -0700

    Ensure text wrapping breaks lines.

--- WordWrapping.UnitTests/WordWrap.cs
+++ WordWrapping.UnitTests/WordWrap.cs
@@ -4,13 +4,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace WordWrapping.UnitTests
 {
     [TestClass]
-    public class Wrapping_Should
+    public class WordWrap_Should
     {
+        // Integration test     
         [TestMethod]
         public void Break_Text()
-        {
+        {            
             var given = "The quick brown fox jumped over the lazy dog.";
-            var expected = "The quick brown\nfox jumped over\nthe lazy dog.";
+
+            var nl = Environment.NewLine;
+            var expected = $"The quick brown{nl}fox jumped over{nl}the lazy dog.";
+
             Assert.AreEqual(expected, given.Wrap(15));
         }
     }
